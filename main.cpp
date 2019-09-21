9ac43e459b27a2e2794b4a7eef4c1ba8e20b04a0
Stypox <stypox@pm.me>
2019-09-21 23:52:30 +0200

    Improve main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "fabio2.cpp"
 #include "giovanni.cpp"
 
+enum class TestMode { ones, random, time };
+constexpr TestMode mode = TestMode::ones;
 constexpr si maxv = 11, maxn = 3;
 
 vec<si> getVals(si N) {
@@ -38,42 +40,23 @@ void time(F function, Args... args) {
 }
 
 int main() {
-	// vec<int> nums{123, 45687, 789, 11, 7};
-	// radix_sort(nums.begin(), nums.end());
-	// for (auto n : nums) cout << n << " "; cout << "\n";
-	// return 0;
-
-	// for (int n = 100;; n += 100) {
-	// 	chrono::time_point tp1 = chrono::high_resolution_clock::now();
-	// 	si res = fabio(vec<si>(n, 1));
-	// 	chrono::time_point tp2 = chrono::high_resolution_clock::now();
-
-	// 	float time = chrono::duration_cast<chrono::microseconds>(tp2-tp1).count() / 1000000.f;
-	// 	cout << "n =" << setw(5) << n <<
-	// 		" time = " << setprecision(time < 1 ? 5 : 6) << time <<
-	// 		"  -->  result = " << res << "\n";
-	// }
-
-	for(int n = 100;; n+=100) {
-		cout<<"MIGLIORATO --> n = "<<n<<" ";
-		time(fabio2, vec<si>(n, 1));
-		cout<<"ORIGINALE  --> n = "<<n<<" ";
-		time(fabio, vec<si>(n, 1));
-		cout<<"\n";
+	switch (mode) {
+		case TestMode::ones:
+			for(int i=1;; ++i)
+				test(vec<si>(i,1));
+			break;
+		case TestMode::random:
+			while(1)
+				test(getVals(rand() % maxn + 1));
+			break;
+		case TestMode::time:
+			for(int n = 100;; n+=100) {
+				cout<<"MIGLIORATO --> n = "<<n<<" ";
+				time(fabio2, vec<si>(n, 1));
+				cout<<"ORIGINALE  --> n = "<<n<<" ";
+				time(fabio, vec<si>(n, 1));
+				cout<<"\n";
+			}
+			break;
 	}
-
-
-	// while (1) {
-	// 	vec<si> vals = getVals(maxn);
-
-	// 	for (auto v : vals) cout << v << " ";
-	// 	cout << flush;
-
-	// 	si fabio_res = fabio(vals);
-	// 	cout << "\nfabio: " << fabio_res << flush;
-
-	// 	si giovanni_res = giovanni(vals);
-	// 	cout << " -- giovanni: " << giovanni_res << "\n\n" << flush;
-	// 	assert(fabio_res==giovanni_res);
-	// }
 }
